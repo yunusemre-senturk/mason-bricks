da859a488fd25f83795b416360ff440f2b5379a4
Yunus Emre Şentürk <yunusntrk53@hotmail.com>
2023-11-03 09:58:52 +0300

    folder_desing_and_add_model

---
+++ bricks/model/__brick__/lib/app/data/model/{{modelName.snakeCase()}}/{{modelName.snakeCase()}}.dart
@@ -0,0 +1,17 @@
+// Package imports:
+import 'package:json_annotation/json_annotation.dart';
+
+// Project imports:
+part '{{modelName.snakeCase()}}.g.dart';
+
+@JsonSerializable()
+class {{modelName.pascalCase()}} {
+  
+  {{modelName.pascalCase()}}({});
+
+  factory {{modelName.pascalCase()}}.fromJson(Map<String, dynamic> json) {
+    return _${{modelName.pascalCase()}}FromJson(json);
+  }
+
+  Map<String, dynamic> toJson() => _${{modelName.pascalCase()}}ToJson(this);
+}
